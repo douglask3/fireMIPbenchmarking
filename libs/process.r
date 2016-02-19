1c996ef3ffd07a8f39c200a41963b8d0ee65fa84
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-02-19 20:45:06 +0000

    add template for processing when opeing files

---
+++ libs/process.r
@@ -0,0 +1,29 @@
+
+process.CLM <- function(dir) {
+    browser()
+}
+
+process.CTEM <- function(dir) {
+    browser()
+
+}
+
+process.inferno <- function(dir) {
+    browser()
+
+}
+
+process.jsbach <- function(dir) {
+    browser()
+
+}
+
+process.lpj <- function(dir) {
+    browser()
+
+}
+
+process.orchidee <- function(dir) {
+    browser()
+
+}
