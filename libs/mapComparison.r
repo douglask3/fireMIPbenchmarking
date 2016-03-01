18012065719b0456630d23b119deb3c68778efe4
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-03-01 19:09:30 +0000

    plotting two new mask plots: 1) reas on simulated outside of observations; 2) areas of observatins outside of model

--- libs/mapComparison.r
+++ libs/mapComparison.r
@@ -23,22 +23,40 @@ mapComparison <- function(obs, mod, name, ...) {
     ## Set up plot
     plotDims   = standardPlotDims(length(mod) + 1)
 
-    pdf('figs/maskMaps.pdf', height = plotDims[2]*3, width = plotDims[1]*5)
 
-    ## Plot
-        par(mfrow = standardPlotDims(length(mod) + 1),
-            mar = rep(0,4), oma = c(0, 0, 3, 0))
+    makePlot <- function(mod, fname) {
+        pdf(fname, height = plotDims[2]*3, width = plotDims[1]*5)
 
-        plotMaskMap <- function(dat, col, name) {
-            if (length(dat) == 0) plot.new() else {
-                plot_raster_from_raster(dat, limits = 0.5, col = c(col, 'white'),
-                                        quick = TRUE, add_legend = FALSE)
+        ## Plot
+            par(mfrow = standardPlotDims(length(mod) + 1),
+                mar = rep(0,4), oma = c(0, 0, 3, 0))
+
+            plotMaskMap <- function(dat, col, name) {
+                if (length(dat) == 0) plot.new() else {
+                    plot_raster_from_raster(dat, limits = 0.5, col = c(col, 'white'),
+                                            quick = TRUE, add_legend = FALSE)
+                }
+                mtext(name, line = -2)
             }
-            mtext(name, line = -2)
-        }
 
-        plotMaskMap(obs, 'grey', 'GFED observations')
+            plotMaskMap(obs, 'grey', 'GFED observations')
 
-        mapply(plotMaskMap, mod, Model.plotting[, 2], Model.plotting[, 1])
-    dev.off.gitWatermark(x = 0.85, y = 0.05, srt = 0, cex = 1.5)
+            mapply(plotMaskMap, mod, Model.plotting[, 2], Model.plotting[, 1])
+        dev.off.gitWatermark(x = 0.85, y = 0.05, srt = 0, cex = 1.5)
+    }
+
+    makePlot(mod, 'figs/maskMaps.pdf')
+
+    mod[index]  = lapply(mod[index], raster::resample, obs)
+
+    modi = mod
+    modi[index] = lapply(modi[index], function(i) i > 0.5 | obs == 0)
+    makePlot(modi, 'figs/maskMaps2.pdf')
+
+    modi = mod
+    modi[index] = lapply(modi[index], function(i) (i < 0.5) | obs != 0)
+    makePlot(modi, 'figs/maskMaps3.pdf')
+
+    
+    browser()
 }
