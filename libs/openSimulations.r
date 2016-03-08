d10040edc6aea55cd9bbe007ea94d2d825fb7fdf
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-03-08 15:56:13 +0000

    removing not required temp fig test

--- libs/openSimulations.r
+++ libs/openSimulations.r
@@ -6,12 +6,6 @@ openSimulations <- function(name, varnN,  ...)
 openSimulation <- function(modInfo, rawInfo, name, varnN, layers) {
     varInfo = Model.Variable[[1]][,varnN]
     modInfo = modInfo[,varnN]
-
-    tempName = paste(c(temp_dir, '/', name, varInfo, modInfo, layers, '.nc'),
-                     collapse = '')
-
-    if (file.exists(tempName)) return(brick(tempName))
-
     dat = process.RAW(varInfo, modInfo, rawInfo, layers)
     return(dat)
 }
