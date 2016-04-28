53924dd23627f26b0bb2b027229b029aa5b5d0da
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-04-28 09:59:27 +0100

    swithcing git back to bitbucket for now

--- initalise.r
+++ initalise.r
@@ -2,7 +2,7 @@ install.packages('devtools')
 library(devtools)
 
 install_github('douglask3/benchmarkmetrics/benchmarkMetrics')
-install_github('douglask3/gitProjectExtras/gitBasedProjects')
+install_bitbucket('douglask3/gitProjectExtras/gitBasedProjects')
 install_bitbucket('douglask3/rasterExtraFuns/rasterExtras')
 install_bitbucket('douglask3/rasterExtraFuns/rasterPlotFunctions')
 install.packages('raster')
