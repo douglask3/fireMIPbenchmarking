4aafd9453492e6ea75a864e62801f31378eee8b7
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-04-28 10:02:24 +0100

    combining mem sdafe cropping into a function. This function should be moved into project rasterExtra at some point

--- libs/cropBothWays.r
+++ libs/cropBothWays.r
@@ -2,11 +2,13 @@ cropBothWays <- function(obs, mod) {
     index = !sapply(mod, is.null)
     modi = mod[index]
 
-    for (i in modi) obs = memSafeFile.crop(obs, i)
+    cropMe <- function(...) memSafeFile.crop(..., overwrite = TRUE)
+
+    for (i in modi) obs = cropMe(obs, i)
 
     cropFun <- function(i, ...) {
-        if (nlayers(i) == 1) return(memSafeFile.crop(i[[1]], obs))
-        return(memSafeFile.crop(i, obs))
+        if (nlayers(i) == 1) return(cropMe(i[[1]], obs))
+        return(cropMe(i, obs))
     }
 
     modi = lapply(modi, cropFun, obs)
