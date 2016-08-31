cc379ae711326925d4dbcd6f0400f153c7022fde
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-08-31 13:30:55 +0100

    allowing overwritre of old temp filesin mem safe function

--- src/libs/memSafeFun.r
+++ src/libs/memSafeFun.r
@@ -3,7 +3,7 @@ memSafeFunction <- function(x, FUN, ...) {
     FUN2 = function(i) {
         l = FUN(i, ...)
         if(!is.raster(l)) return(l)
-        l = writeRaster(l, file = memSafeFile())
+        l = writeRaster(l, file = memSafeFile(), overwrite = TRUE)
     }
 
     x = layer.apply(x, FUN2)
