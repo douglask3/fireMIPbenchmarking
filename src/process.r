ebb1282cc52dc44c954376fe2ec414200d6001d9
douglask3 <douglas.i.kelley@gmail.com>
2016-09-16 08:10:00 +0100

    coberting levels into numeric ready for reading in model dayta

--- src/process.r
+++ src/process.r
@@ -5,7 +5,8 @@ process.RAW <- function (rawInfo, varInfo, modInfo, modLayers, layersIndex,
 
     dir   = paste(data_dir.ModelOutputs, rawInfo[[1]], experiment, sep = '/')
     files = list.files(dir, full.names = TRUE, recursive = TRUE)
-
+    levels = findModelLevels(modInfo[5])
+    
     memSafeFile.initialise('temp/')
         dat = rawInfo[[2]](files, varName = modInfo[1], levels = modInfo[5],
                            startYear = rawInfo[3], modLayers, layersIndex,
@@ -18,6 +19,35 @@ process.RAW <- function (rawInfo, varInfo, modInfo, modLayers, layersIndex,
     return(dat)
 }
 
+findModelLevels <- function(levels) {
+    
+    levels = strsplit(levels, ';')[[1]]#
+    
+    findRange <- function(j) {
+        j = strsplit(j, ':')[[1]]
+        j = as.numeric(j)
+        
+        ln = length(j)
+        if (ln > 3) warning('incorrect levels definition. Check your cfg file')
+        if (ln > 1) j = j[1]:j[2]
+        
+        return(j)
+    }
+    
+    findItemLevels <- function(i) {   
+        if (substr(i, 1, 1) == '-') {
+            i = substr(i, 2, nchar(i))
+            neg = TRUE
+        } else neg = FALSE
+        
+        i = strsplit(i, ',')[[1]]
+        i = sapply(i, findRange)
+        if (neg) i = -i
+        return(i)
+    }
+    levels = lapply(levels, findModelLevels)
+    return(levels)
+}
 
 ################################################################################
 ## Scaling Funs                                                               ##
