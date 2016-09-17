0e41a03ac69c0092460e1d256165387beae424b7
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-09-17 22:01:45 +0100

    splitting cfg to make things a bit more orgiansied. Now run by "source("cfg.r")"

---
+++ cfg/B.modelInfo.r
@@ -0,0 +1,28 @@
+################################################################################
+## Model I/O                                                                  ##
+################################################################################
+
+Model.RAW = list(      #DIR                 #Processing
+            CLM      = c('CLM'               , process.CLM     ),
+            CTEM     = c('CTEM'              , process.CTEM    ),
+            INFERNO  = c('Inferno'           , process.INFERNO ),
+            JSBACH   = c('JSBACH'            , process.default ),
+            LPJglob  = c('LPJ-GUESS-GlobFIRM', process.default ),
+            LPJspit  = c('LPJ-GUESS-SPITFIRE', process.default ),
+            LPJblze  = c('LPJ-GUESS-BLAZE'   , process.default ),
+            MC2      = c('MC2'               , process.default ),
+            ORCHIDEE = c('ORCHIDEE'          , process.orchidee))
+
+################################################################################
+## Model plotting                                                             ##
+################################################################################
+Model.plotting = rbind( #Title            #Colour
+            CLM      = c('CLM'               , 'red'        ),
+            CTEM     = c('CTEM'              , 'green'      ),
+            INFERNO  = c('inferno'           , 'blue'       ),
+            JSBACH   = c('JSBACH'            , 'yellow'     ),
+            LPJglob  = c('LPJ-GUESS-GlobFIRM', 'cyan'       ),
+            LPJspit  = c('LPJ-GUESS-SPITFIRE', 'darkcyan'   ),
+            LPJblze  = c('LPJ-GUESS-BLAZE'   , 'dodgerblue4'),
+            MC2      = c('MC2'               , 'darkgoldenrod4' ),
+            ORCHIDEE = c('ORCHIDEE'          , 'magenta'    ))
