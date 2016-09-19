80fc7d7e2c64c3fb2eeb123702a6e1977280af9d
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-09-19 14:07:07 +0100

    correcting typo

--- src/plotting/plotStandard.r
+++ src/plotting/plotStandard.r
@@ -1,5 +1,5 @@
 plotStandardMap <- function(x, txt, limits, cols, ...) {
-    plot_raster_from_raster(x, limits = limits, cols = cols,
+    plot_raster_from_raster(x, y_range = c(-60, 90), limits = limits, cols = cols,
                             transpose = FALSE, srt = 0,
                             plot_loc = c(0.35,0.83,0.01,0.04),
                             quick = TRUE, ...)
