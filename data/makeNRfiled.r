63516a027abae12d403e7b723db12d0304515d63
douglask3 - Reading <douglas.i.kelley@gmail.com>
2017-01-30 16:42:21 +0000

    making nr files

---
+++ data/makeNRfiled.r
@@ -0,0 +1,16 @@
+## run from within data
+
+library(raster)
+
+fname = 'benchmarkData/1deg_nr_fire.txt'
+fnameOut = 'benchmarkData/NRfire'
+dat = read.csv(fname, sep = '\t')
+
+rasterizeCol <- function(i) {
+    dat[,i] = dat[,i] / dat[, 'gridsize']
+    r = rasterFromXYZ(dat[, c(1,2,i)])
+    fnameOut = paste(fnameOut, colnames(dat)[i], '.nc', sep ='')
+    writeRaster.gitInfo(r, fnameOut, overwrite = TRUE)
+}
+
+rs = lapply(3:5, rasterizeCol)
