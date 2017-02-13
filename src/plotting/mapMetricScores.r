a278fff2372ae26c9b704101a342a3a7ae732c2e
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-02-13 20:40:47 +0000

    plotting metric combo map as ''good'', ''bad'', ''mixed''

--- src/plotting/mapMetricScores.r
+++ src/plotting/mapMetricScores.r
@@ -3,54 +3,59 @@ mapMetricScores <- function(comp, name, info) {
 	scores  = lapply(select2ndCommonItem(comp, 1), score)
 	nmetric =  max(sapply(dat, length), na.rm = TRUE)
 	
-	print("feed in score")
-	browser() 
-	lapply(1:nmetric, mapMetricScore, dat, scores)
-	browser()
+	fname = paste(figs_dir, name, "MetricMapCombo", ".pdf", sep = '-')
+	
+	pdf(fname, height = 5 * nmetric, width = 5)
+	
+	layout(c((1:nmetric)+1, 1))
+	par(mar = rep(0,4))
+	
+	mapply(mapMetricScore, 1:nmetric, c(T, rep(F, nmetric - 1)), MoreArgs = list(dat, scores))
+	dev.off.gitWatermarkStandard()
 
 }
 
-mapMetricScore <- function(i, dat, scores) {
+mapMetricScore <- function(i, pltLegend = FALSE, dat, scores) {
 	dat   = select2ndCommonItem(dat  , i)
 	score = select2ndCommonItem(scores, i)
 	dat   = mapply('/', dat, score)
 	dat   = list2layers(dat)
 	
-	mn      = mean(dat)
-	mn_cols = c('white', '#FF9944', '#FF0000')
-	mn_lims = quantile(mn, c(0.33, 0.67))
+	mn      = mean(dat)	
+	mn_lims = quantile(mn, seq(0.1, 0.9, 0.1))
+	vr      = sd.raster(dat, FALSE)
+	vr[is.na(mn)] = NaN
+	vr_lims = quantile(vr, seq(0.1, 0.9, 0.1))
 	
-	vr      = sd.raster(dat) / mn
-	vr_lims = quantile(vr, c(0.33, 0.67))
-	
-	plot_raster_from_raster(dat, cols = mn_cols, 
-        limits = mn_lims, y_range = c(-60, 90), 		
-        smooth_image = FALSE, smooth_factor = NULL,
-        add_legend = FALSE, 
-        e = vr, limits_error = vr_lims,  
-        ePatternRes = 30,  ePatternThick = 0.2, e_polygon = FALSE)
-	
-	plot.new()
-	#lines(c(0.33, 0.33), c(0, 1))
-	#lines(c(0.67, 0.67), c(0, 1))
-	#lines(c(0, 1), c(0.33, 0.33))
-	#lines(c(0, 1), c(0.67, 0.67))
-	lapply(1:3, function(i) {
-		i2 = i/3; i1 = i2 - (1/3);
-		polygon(c(i1, i2, i2, i1), c(0, 0, 1, 1), col = mn_cols[i], border = NA)
+	cutPlt = mn
+	cutPlt[] = 0.0	
+	cols = c()
+	p = 0
+	if (pltLegend) plot.new()
+	for (i in 1:length(mn_lims)) for (j in length(vr_lims):1) {
+		p = p + 1
+		ml = mn_lims[i]; vl = vr_lims[j]
+		b = (i-1)/(length(mn_lims) - 1)
+		r = (1 - b)
+		g = ((j - 1)/(length(vr_lims) - 1)) ^ 3
+		r = r + (1 - r) * g
+		b = b + (1 - b) * g
+		cols[p] = hex(RGB(r,g,b))
+		test = mn < ml & vr < vl;
+		cutPlt[test] = cutPlt[test] + 1	
 		
-		x = seq(0, 1, by = 0.1) 
-		y = seq(1, i1, by = -0.15)
-		
-		x = rep(x, each = length(y))
-		y = rep(y, length.out = length(x))
-		points(x, y, pch = 16, cex = i, col = make.transparent('black', 1 - i1))
-		})			
-	#lines(c(0.67, 0.0), c(0, 0.67))
-	#lines(c(1, 0.33), c(0.33, 1))
-	text('All models perform well', x = 0.15, y = 0.8)
-	text('Some models perform well', x = 0.5, y = 0.2)
-	text('All models perform poorly', x = 0.85, y = 0.8)
+		x = 1 - (i - 0.5)/length(mn_lims)
+		y = 1 - (j - 0.5)/length(vr_lims)
+		if (pltLegend) points(x, y, pch = 15, col = cols[p])
+	}
+	text(0.2, 0.9, 'All model perform well')
+	text(0.8, 0.9, 'All model perform poorly')
+	text(0.1, 0.5, 'Some models perform well')
+	
+	cutPlt[is.na(mn)] = NaN
+	plot_raster_from_raster(cutPlt, limit = 1:p, cols = cols, y_range = c(-60, 90),
+	                        readyCut = TRUE, add_legend = FALSE)
+							
 }
 
 select2ndCommonItem <- function(lst, i) lapply(lst, function(j) j[[i]])
