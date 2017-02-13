31263c9232864d0ef656b871626cb8e5bfc48531
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-02-13 13:38:53 +0000

    updating for windows path types

--- src/libs/filename.noPath.r
+++ src/libs/filename.noPath.r
@@ -1,5 +1,6 @@
 filename.noPath <- function(r, noExtension = FALSE) {
     fname = filename(r)
+    fname = tail(strsplit(fname, '\\', fixed = TRUE)[[1]], 1)
     fname = tail(strsplit(fname, '/')[[1]], 1)
     if (noExtension)
         fname = paste(head(strsplit(fname, '.', TRUE)[[1]], -1), collapse = '.')
