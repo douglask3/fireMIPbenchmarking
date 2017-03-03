ca3dfd3dd32733463190f21f2403fba5b6a3fd58
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-03-03 22:59:23 +0000

    removing fatal case where trying to apply mask where one is (hopeflly) not needed and not provided

--- src/processModelOutputs/CTEM.r
+++ src/processModelOutputs/CTEM.r
@@ -20,9 +20,8 @@ process.CTEM.level <- function(levels, files, varName, startYear,
     if (openFrac_test) {
         file = findAfile(files, vegVarN)
         veg0 = brickLevels()
-    }
-	
-    mask = !is.na(veg0[[1]][[1]])
+		mask = !is.na(veg0[[1]][[1]])
+    } else mask = NULL
 
     noNaN <- function(dat) {
         dat[is.na(dat)] = 0
@@ -35,10 +34,10 @@ process.CTEM.level <- function(levels, files, varName, startYear,
 
         dat = noNaN(dat0[[1]][[i]]) * v1
         for (j in 2:(length(levels))) {
-            if (!openFrac_test) v2 = 1 else v2 = veg0[[j]][[i]]
+            if (openFrac_test) v2 = veg0[[j]][[i]] else v2 = 1
             dat = dat + noNaN(dat0[[j]][[i]]) * v2
         }
-        dat[is.na(mask)] = NaN
+        if (!is.null(mask)) dat[is.na(mask)] = NaN
         dat = convert_pacific_centric_2_regular(dat)
         return(dat)
     }
