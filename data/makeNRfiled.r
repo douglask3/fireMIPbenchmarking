3b712403bed416d0139f16a62e728d200e722567
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-03-26 12:28:44 +0100

    updating fire size/nr to global

--- data/makeNRfiled.r
+++ data/makeNRfiled.r
@@ -2,15 +2,15 @@
 
 library(raster)
 
-fname = 'benchmarkData/1deg_nr_fire.txt'
+fname = 'benchmarkData/firesize_niels.txt'
 fnameOut = 'benchmarkData/NRfire'
 dat = read.csv(fname, sep = '\t')
 
 rasterizeCol <- function(i) {
-    dat[,i] = dat[,i] / dat[, 'gridsize']
+    dat[,i] = dat[,i] #/ dat[, 'gridsize']
     r = rasterFromXYZ(dat[, c(1,2,i)])
     fnameOut = paste(fnameOut, '-',colnames(dat)[i], '.nc', sep ='')
     writeRaster.gitInfo(r, fnameOut, overwrite = TRUE)
 }
 
-rs = lapply(3:5, rasterizeCol)
+rs = lapply(3:4, rasterizeCol)
