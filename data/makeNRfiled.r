841b20b7abe94c537195af8aaf3fe5956d3e561a
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-03-30 09:25:28 +0100

    weighing no fires by area

--- data/makeNRfiled.r
+++ data/makeNRfiled.r
@@ -6,11 +6,13 @@ fname = 'benchmarkData/firesize_niels.txt'
 fnameOut = 'benchmarkData/NRfire'
 dat = read.csv(fname, sep = '\t')
 
-rasterizeCol <- function(i) {
+rasterizeCol <- function(i, areaTest) {
     dat[,i] = dat[,i] #/ dat[, 'gridsize']
     r = rasterFromXYZ(dat[, c(1,2,i)])
+	if (areaTest) r = r / area(r)
+	
     fnameOut = paste(fnameOut, '-',colnames(dat)[i], '.nc', sep ='')
     writeRaster.gitInfo(r, fnameOut, overwrite = TRUE)
 }
 
-rs = lapply(3:4, rasterizeCol)
+rs = mapply(rasterizeCol, 3:4, c(F, T))
