7b391f8551b1184db0655d22c9773cb387bcfb74
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-04-12 16:16:10 +0100

    splitting to subctigory plots

--- plotByModeType.r
+++ plotByModeType.r
@@ -4,14 +4,16 @@
 source("cfg.r")
 
 #					  CLM    CTEM    INFERNO     JSBACH       LPJglob    LPJSpit,   Blaze    MC2         ORCHIDEE
-ModelSplit = list(
+ModelSplits = list(vegModel = list(
 	 VegModelGroup = c('CLM'       , 'CTEM' , 'JULES'    , 'JSBACH'  , 'LPJ'     ,   'LPJ'   , 'LPJ'    , 'MC2'    , 'ORCHIDEE'),
-	Dymanic        = c('P'         , 'P'    ,'SD'        , 'P'       , 'D'       , 'D'       , 'D'      , 'D'      , 'P'       ),
+	Dymanic        = c('P'         , 'P'    ,'SD'        , 'P'       , 'D'       , 'D'       , 'D'      , 'D'      , 'P'       )),
+				  fireModel = list(
 	FireModelGroup = c('Li '       , 'CTEM' , 'INFERNO'  , 'SPITFIRE', 'GLOBFIRM', 'SPITFIRE', 'SIMFIRE', 'MC-fire', 'SPITFIRE'),
-	Empirical      = c('P'         , 'P'    , 'SP'       , 'P'       , 'E'       , 'P'       , 'E'      , 'SP'     , 'P'       ),
+	Empirical      = c('P'         , 'P'    , 'SP'       , 'P'       , 'E'       , 'P'       , 'E'      , 'SP'     , 'P'       )),
+				  modelRes = list(
 	SpatialRes     = c(2           , 1      , 4          , 3         , 1         ,  1        , 1        , 1        , 1         ),
 	TempralRes     = c('Sub-daily' , 'Daily', 'Sub-daily', 'Daily'   , 'Annual'  , 'Daily'   , 'Annual' , 'Monthly', 'Daily'   ))  
-	
+	)
 	
 maxY = 2
 
@@ -21,12 +23,13 @@ colourSelectFun <- function(cats)
 ################################
 ## plotting function	      ##
 ################################
-compModelCatigory <- function(dat, addXaxis = FALSE, addYaxis = FALSE) {
+compModelCatigory <- function(dat, addXaxis = FALSE, addYaxis = FALSE, ModelSplit) {
 	################################################
 	## Grab params  						      ##
 	################################################
 	compName = dat[[1]]
 	scores   = dat[[2]]
+	nAxis    = length(ModelSplit)
 	
 	nullMean =  mean(as.numeric(scores[, 1]), na.rm = TRUE)
 	nullRR   = scores[, 2]
@@ -49,7 +52,7 @@ compModelCatigory <- function(dat, addXaxis = FALSE, addYaxis = FALSE) {
 	################################################
 	## set up plot frame					      ##
 	################################################
-	plot(c(0, nAxis), c(minY, maxY), type = 'n', yaxt = 'n', xaxt = 'n', xlab = '', log = log)
+	plot(c(0.8, nAxis + 0.2), c(minY, maxY), type = 'n', yaxt = 'n', xaxt = 'n', xlab = '', log = log)
 	mtext(compName)
 	if (addXaxis) labels = names(ModelSplit) else labels = rep('', nAxis)
 	axis(1, at = 1:nAxis, labels = labels, las = 2)
@@ -81,13 +84,13 @@ compModelCatigory <- function(dat, addXaxis = FALSE, addYaxis = FALSE) {
 		catMax  = catPnt(max)
 		
 		
-		x = seq(x - 0.1, x + 0.1, length.out = length(cats))
+		x = seq(x - 0.02, x + 0.02, length.out = length(cats))
 		points(x, catMean, col = cols, pch = 19)
 		
 		addLines <- function(x, mn, mx, col) {
 			lines(c(x, x), c(mn, mx), col = col)
-			lines(c(x - 0.002, x + 0.002), c(mn, mn), col = col)
-			lines(c(x - 0.002, x + 0.002), c(mx, mx), col = col)
+			lines(c(x - 0.01, x + 0.01), c(mn, mn), col = col)
+			lines(c(x - 0.01, x + 0.01), c(mx, mx), col = col)
 		}
 		
 		mapply(addLines, x, catMin, catMax, cols)
@@ -106,7 +109,7 @@ compModelCatigory <- function(dat, addXaxis = FALSE, addYaxis = FALSE) {
 	}
 	
 	steps = 3:ncol(scores)
-	if (length(steps) == 3) offsets = c(-0.25, 0, 0.25)
+	if (length(steps) == 3) offsets = c(-0.2, 0, 0.2)
 	else if (length(steps) == 1) offsets = 0
 	else browser()
 	
@@ -152,45 +155,57 @@ files = list.files('outputs/',  full.names = TRUE)
 files = files[grep('.csv', files)]
 for (i in files) dat = c(dat, openFile(i))
 
-#############################
-## plot                    ##
-#############################
-## setup
-nplots = length(dat)
-nplotsX = floor(sqrt(nplots))
-nplotsY = ceiling(sqrt(nplots))
-nAxis = length(ModelSplit)
-if (nplots > (nplotsX * nplotsY)) nplotsX = nplotsX + 1
-nplotsY = nplotsY + 1
-
-pdf("figs/catigoryScores.pdf", height = nplotsX * 4, width = nplotsY * 2.5)
-
-lmat = t(matrix(1:(nplotsX *nplotsY), nrow = nplotsY))
-lmat[lmat > nplots] = 0.0
-lmat[nplotsX, ] = nplots + 1
-layout(lmat)
-par(mar = c(1, 1, 2, 0), oma = c(1, 2, 1,0))
-
-##performs
-addYaxis = addXaxis = rep(FALSE, nplots)
-addYaxis[seq(1, nplots, nplotsY)] = TRUE
-addXaxis[(nplots - nplotsX):nplots] = TRUE
-
-mapply(compModelCatigory, dat, addXaxis, addYaxis)
-
-## turn off
-
-
-addLegends <- function(cats, y, title) {
-	cats = unique(cats)
-	cols = colourSelectFun(cats)
+plotSpit <- function(ModelSplit, name) {
+	#############################
+	## setup plot              ##
+	#############################
+	nplots = length(dat)
+	nplotsX = floor(sqrt(nplots))
+	nplotsY = ceiling(sqrt(nplots))
 	
-	legend(x = 0.5, xjust = 0.5, y = y, legend = cats, title = title,
-	       horiz = TRUE, pch = 19, col = cols, xpd = TRUE, bty = 'n', lwd = 1, lty = 1)
+	if (nplots > (nplotsX * nplotsY)) nplotsX = nplotsX + 1
+	nplotsY = nplotsY + 1
+
+	fname = paste("figs/catigoryScores", name, "pdf", sep = '.')
+	pdf(fname, height = nplotsX * 4, width = nplotsY * 2.5)
+
+	lmat = t(matrix(1:(nplotsX *nplotsY), nrow = nplotsY))
+	lmat[lmat > nplots] = 0.0
+	lmat[nplotsX, ] = nplots + 1
+	layout(lmat)
+	par(mar = c(1, 1, 2, 0), oma = c(1, 2, 1,0))
+
+	#############################
+	## perform analysis        ##
+	#############################
+	addYaxis = addXaxis = rep(FALSE, nplots)
+	addYaxis[seq(1, nplots, nplotsY)] = TRUE
+	addXaxis[(nplots - nplotsX):nplots] = TRUE
+
+	mapply(compModelCatigory, dat, addXaxis, addYaxis, MoreArgs = list(ModelSplit = ModelSplit))
+
 	
+
+	#############################
+	## add legend		       ##
+	#############################
+	addLegends <- function(cats, y, title) {
+		cats = unique(cats)
+		cols = colourSelectFun(cats)
+		
+		legend(x = 0.5, xjust = 0.5, y = y, legend = cats, title = title,
+			   horiz = TRUE, pch = 19, col = cols, xpd = TRUE, bty = 'n', lwd = 1, lty = 1)
+		
+	}
+
+	plot.new()
+	ys = seq(0.9, 0.1, length.out =length(ModelSplit))
+	mapply(addLegends, ModelSplit, ys, names(ModelSplit))
+	
+	#############################
+	## Turn off   		       ##
+	#############################
+	dev.off.gitWatermarkStandard()
 }
 
-plot.new()
-ys = seq(0.9, 0.1, length.out =length(ModelSplit))
-mapply(addLegends, ModelSplit, ys, names(ModelSplit))
-dev.off.gitWatermarkStandard()
+mapply(plotSpit, ModelSplits, names(ModelSplits))
