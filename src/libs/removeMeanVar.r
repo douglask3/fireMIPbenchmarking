9eec9e1f860b875740beaf5f87dc8f887f738ecb
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-04-13 16:52:30 +0100

    tidy

--- src/libs/removeMeanVar.r
+++ src/libs/removeMeanVar.r
@@ -8,10 +8,7 @@ wgthdMean.raster <- function(x, w = raster::area(x, na.rm = TRUE))
 	sum(values(x * w), na.rm = TRUE) /
 		sum(values(w), na.rm = TRUE)
 
-#wgthdVar.raster   <- function(x, ...) {
-#	x = abs(x - wgthdMean.raster(x, ...))
-#	return(wgthdMean.raster(x, ...))
-#}
+
 
 wgthdMean.default <- function(x, w = rep(1, length(x)))
 	sum(x * w, na.rm = TRUE) / sum(w, na.rm = TRUE)
@@ -21,12 +18,6 @@ wgthdVar   <- function(x, ...) {
 	return(wgthdMean(x, ...))
 }
 
-#removeMean.raster <- function(mod, obs)
-#	mod - wgthdMean.raster(mod) + wgthdMean.raster(obs)
-
-#removeVar.raster <- function(mod, obs)
-#	mod * wgthdVar.raster(obs) / wgthdVar.raster(mod)
-	
 	
 removeMean <- function(mod, obs, ...)
 	mod - wgthdMean(mod) + wgthdMean(obs)
