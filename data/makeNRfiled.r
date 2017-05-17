bac52223a98d8f29d4f10cf593408187791053e1
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-05-17 12:31:41 +0100

    specifying raster package for area call

--- data/makeNRfiled.r
+++ data/makeNRfiled.r
@@ -9,7 +9,7 @@ dat = read.csv(fname, sep = '\t')
 rasterizeCol <- function(i, areaTest) {
     dat[,i] = dat[,i] #/ dat[, 'gridsize']
     r = rasterFromXYZ(dat[, c(1,2,i)])
-	if (areaTest) r = r / area(r)
+	if (areaTest) r = r / raster::area(r)
 	
     fnameOut = paste(fnameOut, '-',colnames(dat)[i], '.nc', sep ='')
     writeRaster.gitInfo(r, fnameOut, overwrite = TRUE)
