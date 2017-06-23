727d4e37bb1dd99e2d40dd24012a9295b920042f
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-06-23 16:00:00 +0100

    changing seasonal plotting dlimits

--- cfg/params.r
+++ cfg/params.r
@@ -17,7 +17,7 @@ NMEmap_qunatiles  = seq(0.2,0.8,0.2)
 SeasonConcCols    = c('white', '#FF00FF', '#000022')
 SeasonConcDcols   = c('green', 'white', 'red')
 SeasonConcLimits  = c(0, 0.2, 0.4, 0.6, 0.8)
-SeasonConcDlimits = c(-0.2, -0.1, -0.05, 0.05, 0.1, 0.2)
+SeasonConcDlimits = c(-0.5, -0.2, -0.1, -0.05, 0.05, 0.1, 0.2, 0.5)
 
 SeasonPhaseCols    = c('blue','cyan','red', 'orange', 'blue')
 SeasonPhaseDcols   = c("#660066","#0000FF",'white','#FF0000', "#660066")
