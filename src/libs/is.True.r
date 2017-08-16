1381cd596291585318870ae782b7df9677da278b
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-08-16 16:07:37 +0100

    if variable not there, then set to FALSE

--- src/libs/is.True.r
+++ src/libs/is.True.r
@@ -1,5 +1,6 @@
 is.True <- function(x) {
-	test = is.logical(x)
+	test = try(is.logical(x), silent = TRUE)
+	if (class(test) == "try-error") return(FALSE)
     if (!test) return(FALSE)
 	if(length(x) == 0) return(FALSE)
     return(x)
