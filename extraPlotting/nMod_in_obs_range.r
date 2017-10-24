57b83210d78cc2d216ec38d199e4a3b5fc5db551
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-10-24 14:35:19 +0100

    moving code to functions

--- extraPlotting/nMod_in_obs_range.r
+++ extraPlotting/nMod_in_obs_range.r
@@ -22,13 +22,7 @@ plotAgreement <- function(x, txt) {
 	mtext(txt, side = 3, adj = 0.1, line = -1)
 }
 
-
-plotVariable <- function(dat, pltSeason, txt, index = NULL, range) {
-	if (nlayers(dat[[1]]) == 1) index = 1
-	else if (is.null(index)) index = 1:nlayers(dat[[1]])
-	
-	dat[[2]] = dat[[2]][!sapply(dat[[2]], is.null)]
-	
+plotSpatialNmod <- function(dat, txt, index, range) {
 	obs = mean(dat[[1]][[index]])
 	mod = layer.apply(dat[[2]], function(i) mean(i[[index]]))
 
@@ -40,32 +34,44 @@ plotVariable <- function(dat, pltSeason, txt, index = NULL, range) {
 	nmod = mean(mod >= lower & mod <= upper, na.rm = TRUE) * 100
 
 	plotAgreement(nmod, txt[1])
+}
+
+plotSeasonalNmod <- function(dat, txt, index, range) {
+	mnthRange =  6 * (1- 1/range)
+	obs = PolarConcentrationAndPhase(dat[[1]][[index]], phase_units = 'months')
+	mod = lapply(dat[[2]], function(i) PolarConcentrationAndPhase(i[[index]], phase_units = 'months'))
 	
-	if (pltSeason) {
-		obs = PolarConcentrationAndPhase(dat[[1]][[index]], phase_units = 'months')
-		mod = lapply(dat[[2]], function(i) PolarConcentrationAndPhase(i[[index]], phase_units = 'months'))
-		
-		modP = layer.apply(mod, function(i) i[[1]])
-		obsP = obs[[1]]
-		
-		diff = abs(obsP - modP)
-		nmod = diff < mnthRange | diff > 12 - mnthRange	
-		nmod = mean(nmod, na.rm = TRUE) * 100
-		plotAgreement(nmod, txt[2])
-		
-		modP = layer.apply(mod, function(i) i[[2]])
-		obsP = obs[[2]]
-		
-		lower = obsP ^ range
-		upper = obsP ^ (1/range)
-		
-		nmod = mean(modP >= lower & modP <= upper, na.rm = TRUE) * 100
-		plotAgreement(nmod, txt[3])
-	}
+	modP = layer.apply(mod, function(i) i[[1]])
+	obsP = obs[[1]]
+	
+	diff = abs(obsP - modP)
+	nmod = diff < mnthRange | diff > 12 - mnthRange	
+	nmod = mean(nmod, na.rm = TRUE) * 100
+	plotAgreement(nmod, txt[2])
+	
+	modP = layer.apply(mod, function(i) i[[2]])
+	obsP = obs[[2]]
+	
+	lower = obsP ^ range
+	upper = obsP ^ (1/range)
+	
+	nmod = mean(modP >= lower & modP <= upper, na.rm = TRUE) * 100
+	plotAgreement(nmod, txt[3])
+}
+
+
+plotVariable <- function(dat, pltSeason, txt, index = NULL, range) {
+	if (nlayers(dat[[1]]) == 1) index = 1
+	else if (is.null(index)) index = 1:nlayers(dat[[1]])
+	
+	dat[[2]] = dat[[2]][!sapply(dat[[2]], is.null)]
+	
+	plotSpatialNmod(dat, txt, index, range)
+	if (pltSeason) plotSeasonalNmod(dat, txt, index, range)
 }
 
 for (i in range) {	
-	mnthRange =  6 * (1- 1/i)
+	
 	fname = paste('figs/nmodAgreement', '-R', i, '.pdf', sep = '')
 	pdf(fname, height = 8, width = 10)
 	lmat = rbind(cbind(1:3,c(4:5,0)), c(6,6))
