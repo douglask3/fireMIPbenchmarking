a93734d6e9e7be17c82c83b04d36e658138d47d4
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-10-27 11:11:22 +0100

    adding obsrrvated and model ensamble of each variable

--- extraPlotting/nMod_in_obs_range.r
+++ extraPlotting/nMod_in_obs_range.r
@@ -1,87 +1,162 @@
+source('cfg.r')
 names = c('fire', 'production')
 comparisons  = list(c("GFED4s.Spatial",  "GFAS"), c("cveg"))
 plotSeason   =      c(TRUE            ,  FALSE , FALSE)
-titles       = list(c('a) Burnt Area',
-					 'b) Seasonal phase',
-					 'c) Seasonal concentration'),
-										 'd) Emission', 'e) Vegetation Carbon')
+titles       = list(list(
+					     c('a) GFED4s burnt area', 'b) Simulated burnt area', 
+					       'c) Model performance in burnt area'),
+					     c('d) GFED4s seasonal phase', 'e) Simulated seasonal phase',    
+						   'f) Model performance in seasonal phase'),
+						 c('g) GFED4s seasonal concentration',
+  						   'h) Simulated seasonal concentration', 
+						   'i) Model performance in seasonal concentration')),
+					list(
+						 c('j) GFAS fire emissions', 'k) Simulated fire emissions',
+                           'm) Model performance in fire emissions')),		
+					list(
+						 c('n) Avitabile vegetative carbon',
+						   'o) Simulated vegetative carbon',
+						   'p) Model performance in vegetative carbon')))
 
+		   
+scale  = c(12, 12, 1)
+						   
 res = NULL
 openOnly = TRUE
 range = c(1.2, 2.0)
 
-limits = seq(10, 90, 10)
-cols   = c('#AA0000', '#FFFF55', '#008800')
+nmodLims  = seq(10, 90, 10)
+nmodeCols = c('#AA0000', '#FFFF55', '#008800')
 
 source('run.r')
+
+limits = list(GFED4s.Spatial$plotArgs$limits,
+		   GFAS$plotArgs$limits,
+		   cveg$plotArgs$limits)
+		   
+cols   = list(GFED4s.Spatial$plotArgs$cols,
+		   GFAS$plotArgs$cols,
+		   cveg$plotArgs$cols)
+
 if (length(names) > 1) out = unlist(out, recursive = FALSE)
 
-plotAgreement <- function(x, txt) {
+plotAgreement <- function(x, txt, limits = nmodLims, cols = nmodeCols, ...) {
 	plotStandardMap(x, '',  limits = limits, cols = cols,
-					add_legend = FALSE)
+					add_legend = FALSE, e_polygon = FALSE, ePatternRes = 30, 
+					ePatternThick = 0.2, limits_error = c(1, 2), ...)
 	mtext(txt, side = 3, adj = 0.1, line = -1)
 }
 
-plotSpatialNmod <- function(dat, txt, index, range) {
-	obs = mean(dat[[1]][[index]])
-	mod = layer.apply(dat[[2]], function(i) mean(i[[index]]))
-
-	lower = obs / range
-	upper = obs * range
-	
-	upper[upper < 0.001] = 0.001
-
-	nmod = mean(mod >= lower & mod <= upper, na.rm = TRUE) * 100
-
-	plotAgreement(nmod, txt[1])
+plotLegend <- function(cols, limits, plot_loc = c(0.2, 0.3, 0.8, 0.99), ...) {
+	add_raster_legend2(cols = cols, limits = limits, ylabposScling = 2,
+						   transpose = FALSE, plot_loc = plot_loc, 
+						   add = FALSE, ...)
 }
 
+plotSpatialNmod <- function(dat, txt, index, limits, cols, range, scale) {
+	obs = mean(dat[[1]][[index]]) * scale
+	
+	plotAgreement(obs, txt[[1]][1], limits, cols)
+	
+	mod = layer.apply(dat[[2]], function(i) mean(i[[index]]))
+	plotAgreement(mean(mod) * scale, txt[[1]][2], limits, cols, e = sd.raster(mod))
+	
+	lower = obs / range
+	upper = obs * range
+	upper[upper < 0.001] = 0.001
+	nmod = mean(mod >= lower & mod <= upper, na.rm = TRUE) * 100
+
+	plotAgreement(nmod, txt[[1]][3])
+	plotLegend(cols, limits, e_lims = c(1, 2))
+}
+
+
+
 plotSeasonalNmod <- function(dat, txt, index, range) {
+	
 	mnthRange =  6 * (1- 1/range)
 	obs = PolarConcentrationAndPhase(dat[[1]][[index]], phase_units = 'months')
-	mod = lapply(dat[[2]], function(i) PolarConcentrationAndPhase(i[[index]], phase_units = 'months'))
+	mod = lapply(dat[[2]][-c(5,8)], function(i) PolarConcentrationAndPhase(i[[index]], phase_units = 'months'))
+	
+	obsP = obs[[1]]
+	plotAgreement(obsP, txt[[2]][1], SeasonPhaseLimits, SeasonPhaseCols)
+	SeasonLegend(SeasonPhaseLimits, SeasonPhaseCols, dat = modPmean,
+				 mar = rep(0,4), add = TRUE, xp = 1.08, e_lims = c(1,2))
 	
 	modP = layer.apply(mod, function(i) i[[1]])
-	obsP = obs[[1]]
+	
+	xP = mean(cos(modP * 2 * pi / 12), na.rm = TRUE)
+	yP = mean(sin(modP * 2 * pi / 12), na.rm = TRUE)
+	modPmean = atans(yP, xP)
+	modPmean[is.na(obsP)] = NaN
+	
+	xP = sd.raster(cos(modP * 2 * pi / 12))
+	yP = sd.raster(sin(modP * 2 * pi / 12))
+	modPsd = atans(yP, xP)
+	modPsd[is.na(obsP)] = NaN
+	
+	plotAgreement(modPmean, txt[[2]][2], SeasonPhaseLimits, SeasonPhaseCols, e = modPsd / 3)
+	SeasonLegend(SeasonPhaseLimits, SeasonPhaseCols, dat = modPmean, mar = rep(0,4),
+				 add = TRUE, xp = 0.08, e_lims = c(1,2))
 	
 	diff = abs(obsP - modP)
 	nmod = diff < mnthRange | diff > 12 - mnthRange	
 	nmod = mean(nmod, na.rm = TRUE) * 100
-	plotAgreement(nmod, txt[2])
+	plotAgreement(nmod, txt[[2]][3])
 	
-	modP = layer.apply(mod, function(i) i[[2]])
-	obsP = obs[[2]]
+	plot.new()
+	plot.new()
 	
-	lower = obsP ^ range
-	upper = obsP ^ (1/range)
+	obsC = obs[[2]]
+	plotAgreement(obsC, txt[[3]][1], SeasonConcLimits, SeasonConcCols)
 	
-	nmod = mean(modP >= lower & modP <= upper, na.rm = TRUE) * 100
-	plotAgreement(nmod, txt[3])
+	modC  = layer.apply(mod, function(i) i[[2]])
+	modCm = mean(modC, na.rm = TRUE)
+	modCm[is.na(obsP)] = NaN
+	plotAgreement(modCm, txt[[3]][2], SeasonConcLimits, SeasonConcCols, e = sd.raster(modC))
+	
+	lower = obsC ^ range
+	upper = obsC ^ (1/range)
+	
+	nmod = mean(modC >= lower & modC <= upper, na.rm = TRUE) * 100
+	plotAgreement(nmod, txt[[3]][3])
+	
+	plotLegend(SeasonConcCols, SeasonConcLimits, e_lims = c(1, 2))
 }
 
 
-plotVariable <- function(dat, pltSeason, txt, index = NULL, range) {
+plotVariable <- function(dat, pltSeason, txt, 
+						 limits, cols, add_extra_leg, 
+						 index = 1:12, range, ...) {
 	if (nlayers(dat[[1]]) == 1) index = 1
 	else if (is.null(index)) index = 1:nlayers(dat[[1]])
 	
 	dat[[2]] = dat[[2]][!sapply(dat[[2]], is.null)]
 	
-	plotSpatialNmod(dat, txt, index, range)
-	if (pltSeason) plotSeasonalNmod(dat, txt, index, range)
+	plotSpatialNmod(dat, txt, index, limits, cols, range, ...)
+	if (pltSeason) {
+		plot.new()
+		plotSeasonalNmod(dat, txt, index, range)
+	}
+	
+	if (add_extra_leg) plotLegend(nmodeCols, nmodLims, plot_loc = c(0.2, 0.5, 0.8, 0.8))
+	else plot.new()
 }
 
-for (i in range) {	
+for (r in range) {		
+	nrow = length(plotSeason) + sum(plotSeason) * 2
 	
-	fname = paste('figs/nmodAgreement', '-R', i, '.pdf', sep = '')
-	pdf(fname, height = 8, width = 10)
-	lmat = rbind(cbind(1:3,c(4:5,0)), c(6,6))
-	layout(lmat, heights = c(1,1,1,0.3))
-	par(mar = rep(0, 4), oma = c(0, 0, 2, 0))
+	lmat = index = t(matrix(c(1:3,4,4,5), ncol = 2))
+	for (i in 2:nrow) lmat = rbind(lmat, index + (i-1) * 5)
 	
-	mapply(plotVariable, out, plotSeason, titles, MoreArgs = list(range = i))
-
-	add_raster_legend2(cols = cols, limits = limits, ylabposScling = 2,
-	                   transpose = FALSE, plot_loc = c(0.2, 0.6, 0.8, 0.9), add = FALSE)
-					   
+	#lmat = t(matrix(1:(3*6), ncol = nrow))
+	#lmat = rbind(lmat, max(lmat) + 1)
+	
+	fname = paste('figs/nmodAgreement', '-R', r, '.png', sep = '')
+	png(fname, height = 2 * nrow, width = 10, unit = 'in', res = 300)
+		layout(lmat, heights = c(1, 0.3, 1, 0.01, 1, 0.3, 1, 0.3, 1, 0.3))
+		par(mar = rep(0, 4), oma = c(0, 0, 2, 0))
+		
+		mapply(plotVariable, out, plotSeason, titles, limits, cols, c(T, F, F), scale = scale, MoreArgs = list(range = r))			   
 	dev.off.gitWatermarkStandard()
 }
--- src/plotting/seasonLegend.r
+++ src/plotting/seasonLegend.r
@@ -1,7 +1,8 @@
-SeasonLegend <- function(limits, cols, add, plot_loc, dat, e_lims = NULL,
+SeasonLegend <- function(limits, cols, add = FALSE, plot_loc, dat, e_lims = NULL,
                          mnthLabs = 1:12,
                          mar = c(4,0,0,0),labR = 0.87,
                          MonLetters = c('J','F','M','A','M','J','J','A','S','O','N','D'),
+						 xp = 1.0, yp = 0.13, radius = 0.11,
                          ...) {
     
     if (all(range(limits)==c(0,11))) {
@@ -21,38 +22,58 @@ SeasonLegend <- function(limits, cols, add, plot_loc, dat, e_lims = NULL,
 
     x = rep(1,length(cols))
 
-    mar0  = par("mar")
-    par(mar = mar)
+	if (!add || !is.null(mar)) {
+		mar0  = par("mar")
+		par(mar = mar)
+	}
+	
+	if (add) {
+		par(usr = c(0,1,0,0.416666667))
+		add.pie(x, xp, yp, col = cols, clockwise = TRUE,
+		        init.angle = dang + 90 + 180/length(x),
+				radius = radius, labels = labelss, xpd = TRUE)
+		
+	} else {  
+		pie(x,col=cols,labels='',clockwise = TRUE, init.angle = dang + 90 + 180/length(x))
+	
+		angles = dang * pi/180 + head(seq(0,2*pi ,length.out=length(labelss)+1),-1)+pi/2
 
-    pie(x,col=cols,labels='',clockwise = TRUE, init.angle = dang + 90 + 180/length(x))
-
-    angles = dang * pi/180 + head(seq(0,2*pi ,length.out=length(labelss)+1),-1)+pi/2
-
-    pie.labels(0,0,angles,rev(labelss),radius=labR,xpd=NA)
-
+		pie.labels(0,0,angles,rev(labelss),radius=labR,xpd=NA)
+	}
+	
     if (!is.null(e_lims)) {
-        ylims = (seq(0, 0.8, length.out = length(e_lims) + 2)[-1])^1.5
-        ylim0 = 0.8
-        ylims = head(ylims, -1)
-
-        plotElimCircle <- function(i, x, ylim, ...) {
-            lim = sqrt(0.8^2-i^2)
-            if (is.na(lim)) return()
-            xi = x[x<lim & x>(-lim)]
-
-            lim2 = sqrt(ylim - i^2)
-            if (!is.na(lim2)) xi = xi[xi<(-lim2) | xi>lim2]
-            points(xi, rep(i, length(xi)),...)
-        }
-
-        plotElimCirlces <- function(ylim, by, ...) {
-            x = y = seq(-ylim0,ylim0,by = by)
-            lapply(y, plotElimCircle, x, ylim, ...)
-        }
-
-        mapply(plotElimCirlces, ylims,
-               by = seq(0.05,0.1,length.out=length(ylims)), cex = c(0.05,0.2),
-               pch = 19)
+		if (!add) {
+			xp = 0.0
+			yp = 0.0
+			radius = 0.8
+		}
+		
+		x = seq(xp - radius, xp + radius, length.out = 30)
+		y = seq(yp - radius, yp + radius, length.out = 30)
+		e_radius = rev((seq(0, radius, length.out = length(e_lims) + 2))[-1])
+		
+		
+		plot_elim <- function(er, cex, pch, col) {
+			plotYs <- function(y) {
+				xi = c(-1, 1) * sqrt(er^2 - (y  - yp)^2) + xp
+				if (!is.na(diff(xi)) && diff(xi) != 0) {
+					xh = x[x > xi[1] & x < xi[2]]
+					points(xh, rep(y, length(xh)), cex = cex, pch = pch, col = col)
+				}
+			}
+			lapply(y, plotYs)
+		}
+		
+		
+		mapply(plot_elim, e_radius, cex = c(0.0, 1.0, 1.5) * 0.25, pch = 16, col =  make.transparent('black', c(0.33, 0.33, 0.33)))
     }
     par(mar=mar0)
+}
+
+scale2MeanRange <- function(v, mn, rg) {
+	v = v - min(v)	
+	v = v / max(v)
+	v = v - 0.5
+	v = v * rg * 2 + mn
+	return(v)
 }
