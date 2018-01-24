7e0c1f9689baa27e9b3c2f39de37af3aea16ba8a
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-01-24 11:26:24 +0000

    masking for silly high values in obsverations

--- src/openObservations.r
+++ src/openObservations.r
@@ -4,7 +4,7 @@ openObservation <- function(file, varname,...) {
         dat = try(openCsvInputs(file = file, dir = obs_dir, ...), silent = TRUE)
     } else {
         dat = try(openRasterInputs(file = file, varname = varname,
-                                   dir = obs_dir, ...), silent = TRUE)
+                                   dir = obs_dir, check4mask = TRUE, ...), silent = TRUE)
     }
     if (!class(dat) == "try-error") return(dat)
 }
@@ -16,17 +16,27 @@ openCsvInputs <- function(file, layerID = NULL, scaling = NULL, dir) {
     return(dat)
 }
 
-openRasterInputs <- function(file, varname = "", layerID = NULL, scaling = NULL, dir) {
+openRasterInputs <- function(file, varname = "", layerID = NULL, scaling = NULL, dir, check4mask = FALSE) {
     if (is.null(varname)) varname = ""
-    fname = paste(dir, file, sep="")
+    fname = paste(dir, file, sep = "")
     
     dat = layer.apply(varname, function(i) brick(fname, varname = i))
-
+	
     if (nlayers(dat) > 1 && !is.null(layerID)) {
         if(is.list(layerID))
             dat = layer.apply(layerID, function(i) mean(dat[[i]]))
         else dat = dat[[layerID]]
     }
+	
+	if (check4mask) {
+		tempFname = paste(temp_dir, filename.noPath(file, TRUE), "_maskRemoval.nc", sep = "")
+		if (file.exists(tempFname)) {
+			dat = brick(tempFname)
+		} else {
+			dat[dat > 9E9] = NaN
+			dat = writeRaster.gitInfo(dat, tempFname)
+		}
+	}
     if (!is.null(scaling)) dat = scaling(dat)
     return(dat)
 }
