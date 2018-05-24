cfa10bc06111a300f69b8a19359b458e401f82c4
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-05-24 01:22:52 +0100

    opeig tile area weighted varaibles

--- src/processModelOutputs/jules.r
+++ src/processModelOutputs/jules.r
@@ -10,12 +10,15 @@ process.jules <- function(files, varName, levels, startYear, layers, ...) {
 	
 	dat = lapply(levels, process.jules.level, 
 					          files, varName, layers, ...)
+	
+	if(length(dat) == 0 || all(sapply(dat, is.null))) return(NULL)
     return(dat)
 }
 
 process.jules.level <- function(level, files, varName,
                         layers, layersIndex, combine) {
 	r = layer.apply(files, process.jules.file, level, varName)
+	if (is.null(r)) return(NULL)
 	r = r[[layers]]
 	r = combineRawLayers(r, layersIndex, combine)
 	return(r)
@@ -25,6 +28,11 @@ process.jules.file <- function(file, level, varName) {
 	
 	nc = nc_open(file)
 	vars = names(nc$var)
+	
+	if (all(vars != varName)) {
+		noFileWarning(c(), varName)
+		return(NULL)
+	}
 	
 	getVar <- function(var) {
 		var = nc$var[[which(vars == var)]]
@@ -39,24 +47,33 @@ process.jules.file <- function(file, level, varName) {
 	
 	l = length(lat)
 	
-	multiLayer <- function(mn) {
-		mdat = dat[, level, mn]		
-		mdat = apply(mdat,1 , sum)
+	multiLayer <- function(mn, leveli = level) {
+		mdat = dat[, leveli, mn]
+		if (!is.null(dim(mdat)))
+			mdat = apply(mdat,1 , sum)
+		return(mdat)
 	}
 	
 	singleLayer <- function(mn) dat[, mn]
 	
-	monthizeData <- function(mn, FUN) {
-		mdat = FUN(mn)
+	monthizeData <- function(mn, FUN, ...) {
+		mdat = FUN(mn, ...)
 		r = rasterFromXYZ(cbind(lon, lat, mdat))
 		return(r)
 	}
 	
-	if (length(dim(dat)) == 2) FUN = singleLayer
-		else if (length(dim(dat)) == 3) FUN = multiLayer
-		else browser()
-	
-	r = layer.apply(1:12, monthizeData, FUN)
+	if (length(dim(dat)) == 2) r = layer.apply(1:12, monthizeData, singleLayer)
+		else if (length(dim(dat)) == 3) {
+			if (varName != "landCoverFrac") {
+				openWeightLayer <- function(fracLevel, varLevel) {
+					frac = process.jules.file(file, fracLevel, "landCoverFrac")
+					r = layer.apply(1:12, monthizeData, multiLayer, varLevel)
+					frac * r / 100
+				}
+				ri = mapply(openWeightLayer, list(1, 2, 3:5, 6:8, 9), 1:5)
+				r = ri[[1]] + ri[[2]] + ri[[3]] + ri[[4]] + ri[[5]]				
+			} else r = layer.apply(1:12, monthizeData, multiLayer)
+		} else browser()
 	
 	return(r)
 }
