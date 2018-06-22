8384fe6651b9cab2ffe418d1cdd23cefc6f81a66
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-06-22 14:10:13 +0100

    running required comparisons only

--- run.fire.r
+++ run.fire.r
@@ -1,5 +1,5 @@
 names = 'fire'
-comparisons = list(c("GFED4s.Spatial"))
-res = c(0.5, 1, 2, 4, 8, 16)
+comparisons = list(c("GFED4s.Trend", "GFED4s.Spatial", "GFEDsSeason", "meris.Spatial", "MCD45.Spatial"))#list(c("GFASSeason"))
+res = NaN
 
 source('run.r')
