d70654d27d1264ef6a51d83c1bed87be9a1d6f3e
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-03-26 14:14:28 +0000

    correcting typos and getting sample from fireMIP repo

--- data/make_MODIS_BA.r
+++ data/make_MODIS_BA.r
@@ -3,8 +3,7 @@ source("cfg.r")
 
 fname = 'data/MODIS250_q_BA.nc'
 fnameOut = 'data/benchmarkData/MODIS250_q_BA_regridded0.5.nc'
-sample_fname = '../LimFIRE/outputs/fire2000-2014.nc'
-
+sample_fname = 'data/benchmarkData/Fire_GFEDv4_Burnt_fraction_0.5grid9.nc'
 
 dat = brick(fname)
 smp = raster(sample_fname)
@@ -17,5 +16,11 @@ regridScale <- function(r) {
 
 memSafeFile.initialise('temp/')
     dat2 = layer.apply(dat, memSafeFunction, regridScale)
-    dat2 = writeRaster(dat2, file = fnameOut)
+    dat2 = writeRaster.gitInfo(dat2, file = fnameOut,
+                               overwrite = TRUE,
+                               comment = list("original file" = "MODIS250_q_BA.nc",
+                                              "original CDI" = "Climate Data Interface version 1.9.5 (http://mpimet.mpg.de/cdi)",
+                                              "regridded by" = "Douglas Kelley",
+                                              "contact" = "douglas.i.kelley@gmail.com",
+                                              "script file" = "data/make_MODIS_BA.r"))
 memSafeFile.remove()
