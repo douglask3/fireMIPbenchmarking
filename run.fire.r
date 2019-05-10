9403c9856fe43f4384d3778f607d5d9aa933a0bc
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-05-10 09:01:03 +0100

    chaning run fire comparisons

--- run.fire.r
+++ run.fire.r
@@ -1,6 +1,7 @@
 names = 'fire'
 
-comparisons = list(c("GFED4s.","MODIS.IA", "GFED4.Spatial", "GFED4s.Spatial", "meris.Spatial", "MCD45.Spatial", "MODIS.Spatial"))
+comparisons = list(c("GFED4s.IA","MODIS.IA", "GFED4.Spatial", "GFED4s.Spatial", "meris.Spatial", "MCD45.Spatial", "MODIS.Spatial"))
+comparisons = list(c("GFED4s.IA"))
 
 res = NULL
 
