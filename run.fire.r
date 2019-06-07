c43a70f9fd865492349697137d06d2838017f218
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-06-07 17:21:15 +0100

    changing comparisons perfomred

--- run.fire.r
+++ run.fire.r
@@ -1,5 +1,5 @@
 names = 'fire'
-comparisons = list(c("GFED4s.Spatial","MODIS.IA", "GFED4.Spatial", "GFED4s.Spatial", "meris.Spatial", "MCD45.Spatial", "MODIS.Spatial"))
+comparisons = list(c( "GFAS.Spatial", "GFED4s.Spatial", "GFED4s.Trend","GFAS.Trend", "GFED4.Spatial", "meris.Spatial", "MCD45.Spatial", "MODIS.Spatial"))
 res = NULL
 
 source('run.r')
