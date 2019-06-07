1c954e1ad6a80ffa12a58991bd745bfba136d348
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-06-07 17:30:09 +0100

    opening annual as well as monthly datat andallowing summing of variables

--- src/processModelOutputs/jules.r
+++ src/processModelOutputs/jules.r
@@ -1,15 +1,18 @@
 process.jules <- function(files, varName, levels, startYear, layers, ...) {
     files0 = files
     if (noFileWarning(files, varName)) return(NULL)
-	
-    index = unique(ceiling((layers-1)/12)[-1])
+    
+    if (all(grepl('monthly',files))) nm = 12 else nm = 1
+    index = unique(ceiling((layers-1)/nm)[-1])
+    
     files = files[index]
-    layers = layers - ((min(index)-1)*12)
+    
+    layers = layers - ((min(index))*nm)
 	
     if (noFileWarning(files, varName)) return(NULL)
-	
-    dat = lapply(levels, process.jules.level, 
-					          files, varName, layers, ...)
+    	
+    dat = lapply(levels, process.jules.level, files, varName, layers, ...)
+    
     if(length(dat) == 0 || all(sapply(dat, is.null))) return(NULL)
     return(dat)
 }
@@ -18,27 +21,35 @@ process.jules.level <- function(level, files, varName,
                                 layers, layersIndex, combine) {
     r = layer.apply(files, process.jules.file, level, varName)
     if (is.null(r)) return(NULL)
+    
     r = r[[layers]]
     r = combineRawLayers(r, layersIndex, combine)
     return(r)
 }
 
-process.jules.file <- function(file, level, varName) {	
+process.jules.file <- function(file, level, varNames) {	
     nc = nc_open(file)
     vars = names(nc$var)
-	
-    if (all(vars != varName)) {
-        noFileWarning(c(), varName)
-        return(NULL)
-    }
-	
+    varNames  = strsplit(varNames, ';')[[1]]
+
+
     getVar <- function(var) {
         var = nc$var[[which(vars == var)]]
         dat = ncvar_get( nc, var)
         return(dat)
     }
+    openVarN <- function(varName) {
+        if (all(vars != varName)) {
+            noFileWarning(c(), varName)
+            return(NULL)
+        }
 	
-    dat = getVar(varName)
+        dat = getVar(varName)
+    }
+    dat = openVarN(varNames[1])
+    if (length(varNames) > 1) for (i in varNames[-1]) dat = dat + openVarN(i)
+    #dat = sapply(varNames, openVarN)
+
     lat = getVar("latitude")
     lon = getVar("longitude")
     tim = getVar("time_bounds")
@@ -59,19 +70,19 @@ process.jules.file <- function(file, level, varName) {
         r = rasterFromXYZ(cbind(lon, lat, mdat))
         return(r)
     }
-	
-    if (length(dim(dat)) == 2) r = layer.apply(1:12, monthizeData, singleLayer)
-        else if (length(dim(dat)) == 3) {
-            if (varName != "landCoverFrac") {
-                openWeightLayer <- function(fracLevel, varLevel) {
+    if (length(dim(dat)) == 1) r = rasterFromXYZ(cbind(lon, lat, dat)) 
+    else if (length(dim(dat)) == 2) r = layer.apply(1:12, monthizeData, singleLayer)
+    else if (length(dim(dat)) == 3) {
+        if (varName != "landCoverFrac") {
+            openWeightLayer <- function(fracLevel, varLevel) {
                 frac = process.jules.file(file, fracLevel, "landCoverFrac")
                 r = layer.apply(1:12, monthizeData, multiLayer, varLevel)
-                                frac * r / 100
-				}
-                ri = mapply(openWeightLayer, list(1, 2, 3:5, 6:8, 9), 1:5)
-                r = ri[[1]] + ri[[2]] + ri[[3]] + ri[[4]] + ri[[5]]				
-            } else r = layer.apply(1:12, monthizeData, multiLayer)
-        } else browser()
-	
+                frac * r / 100
+	    }
+            ri = mapply(openWeightLayer, list(1, 2, 3:5, 6:8, 9), 1:5)
+            r = ri[[1]] + ri[[2]] + ri[[3]] + ri[[4]] + ri[[5]]				
+        } else r = layer.apply(1:12, monthizeData, multiLayer)
+    } else browser()
+    
     return(r)
 }
