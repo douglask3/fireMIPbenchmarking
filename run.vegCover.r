5a7cc278d4f5c559101784798197fc7b77a4db97
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-06-25 10:50:28 +0100

    mechmarking veg cover

--- run.vegCover.r
+++ run.vegCover.r
@@ -1,3 +1,4 @@
 names = 'vegCover'
-comparisons = list(c("LAImodis"))
+comparisons = list(c("lifeForm2", "TreeCover2", "GrassCover2", 
+                     "BareCover2", "LeafType", "Phenology"))
 source('run.r')
