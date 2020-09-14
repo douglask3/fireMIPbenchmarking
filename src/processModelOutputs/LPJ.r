601350706427933abaf51f6ee9da92d599375f86
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-09-14 12:24:58 +0100

    LPJ LAI processing

--- src/processModelOutputs/LPJ.r
+++ src/processModelOutputs/LPJ.r
@@ -1,57 +1,73 @@
 process.LPJ <- function(files, varName, levels, ...) {
-	if (varName == "lai") dat = process.LPJ.LAI(files, varName, ...)
-		else if (all(is.na(levels)))
-			 dat = process.default(files, varName, levels, ...)
-		else dat = layer.apply(levels, process.LPJ.level, files, varName, ...)
-	return(dat)	
+    
+    if (varName == "lai") dat = process.LPJ.LAI(files, varName, ...)
+	else if (all(is.na(levels)))
+	    dat = process.default(files, varName, levels, ...)
+	else dat = layer.apply(levels, process.LPJ.level, files, varName, ...)
+    
+    return(dat)	
 }
 
 process.LPJ.LAI <- function(files, varName, startYear, layers, ...) {
-	levels = 'all'
-	c(lai, nVtypes) := process.LPJ.level(levels, files, varName, startYear, layers, ...)
-	c(frac, nVtypes) := process.LPJ.level(levels, files, "landCoverFrac", startYear, layers, ...)
-	
-	return(lai)
+    levels = 'all'
+        	
+    c(lai, nVtypes) := process.LPJ.level(levels, files, varName, startYear, layers, ...)
+    c(frac, nVtypes) := process.LPJ.level(levels, files, "landCoverFrac", startYear, layers, ...)
+   
+    LAI = process.laiFracRelayers(lai, frac, layers, nVtypes) 
+    return(LAI)
 }
 
 process.laiFracRelayers <- function(lai, frac, layers, nVtypes) {
-	lai = lai * frac
-	nlay = nlayers(lai) / nVtypes
+    lai = lai * frac
+    nlay = nlayers(lai) / nVtypes
 	
-	combineVtypes <- function(vn) sum(lai[[(1 + (vn - 1) * nVtypes):(vn*nVtypes)]])
-	lai = layer.apply(1:nlay, combineVtypes)
+    combineVtypes <- function(vn) sum(lai[[(1 + (vn - 1) * nVtypes):(vn*nVtypes)]])
+    lai = layer.apply(1:nlay, combineVtypes)
 	
-	index = layers - layers[1] + 1
-	lai = layer.apply(index, function(i) lai[[i]])
-	return(lai)
+    index = layers - layers[1] + 1
+    lai = layer.apply(index, function(i) lai[[i]])
+    return(lai)
 }
 	
 
 process.LPJ.level <- function(levels, files, varName, startYear,
                         layers, layersIndex, combine, nr = 360) {
 	
-	processLayers = unique(layers)
-	brickLevels <- function()
+    processLayers = unique(layers)
+    brickLevels <- function()
         lapply(1:nr, function(i) brick.gunzip(file, level = i)[[processLayers]])
-	file = findAfile(files, varName)
+    
+   
+    file = findAfile(files, varName)
     if (noFileWarning(files, varName)) return(NULL)
-	
-	
-	dat = brickLevels()
+    dat = brick(file)
+    
+    if (ncol(dat) == 720 && nrow(dat) ==360)	{
+        dat = dat[[layers]]
+        return(dat)
+    }
+    if (all(res(dat)==0.5)) {
+        dat = dat[[layers]]
+        dat = raster::extend(dat, extent(c(-180, 180, -90, 90)))
+        return(dat)
+    }
+    browser()
+    dat = brickLevels()
     nc =  nrow(dat[[1]])
-	if (levels == "all") {
-		nlevelJump = ncol(dat[[1]])
-		levels = 1
-		combine = NULL
-	} else nlevelJump = 1
-
+    if (levels == "all") {
+        nlevelJump = ncol(dat[[1]])
+	levels = 1
+	combine = NULL
+    } else nlevelJump = 1
+    
     r = brick(xmn = -180, xmx = 180, ymn = -90, ymx = 90,
                    nrows = nr, ncols = nc, nl = nlevelJump)
 
     buildCell <- function(lat, layer, level) {
-		out = getValuesBlock(t(dat[[lat]][[layer]]), row = level, nrow = nlevelJump)
-		return(rev(out))		
-	}
+	 out = getValuesBlock(t(dat[[lat]][[layer]]), row = level, nrow = nlevelJump)
+	return(rev(out))		
+    }
 	
     buildLayer <- function(...) {
         for (i in 1:nr) r[nr - i + 1, ] = buildCell(i, ...)	
@@ -59,17 +75,16 @@ process.LPJ.level <- function(levels, files, varName, startYear,
     }
 	
     buildLevels <- function(i, ...) {
-		out = layer.apply(1:nlayers(dat[[1]]), buildLayer, i, ...)
+	out = layer.apply(1:nlayers(dat[[1]]), buildLayer, i, ...)
         if (!is.null(combine)) out = combineLayers(out, combine)
-		return(out)
-	}	
+	return(out)
+    }	
 	
+    dat = layer.apply(levels, buildLevels)
+    browser()
+    if (is.null(combine)) return(c(dat, nlevelJump)) 
+    	
+    dat = combineLayers(dat, 'sum')
 	
-	dat = layer.apply(levels, buildLevels)
-	
-	if (is.null(combine)) return(c(dat, nlevelJump)) 
-	
-	dat = combineLayers(dat, 'sum')
-	
-	return(dat)
+    return(dat)
 }
