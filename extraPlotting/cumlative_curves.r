195c59a69f80838340f14f04a34af3caf084ed70
Douglas Kelley <douglas.i.kelley@gmail.com>
2020-11-17 13:31:44 +0000

    adding in cropland pasture cover cumplaitive comparisons

--- extraPlotting/cumlative_curves.r
+++ extraPlotting/cumlative_curves.r
@@ -1,33 +1,36 @@
 
 graphics.off()
 source("cfg.r")
-load("data/gfed4s_2000_2010_fordoug.RData")
-load("data/mcd45_2001_2010_fordoug.RData")
-load("data/meris_2005_2010_fordoug.RData")
+load("data/gfed4s_fcc_gfed4_fordoug.RData")
+load("data/mcd45_fordoug.RData")
+load("data/meris_fordoug.RData")
 
 BAs = list(GFED4 = gfed_ba, GFED4s = gfed4s_ba,
-           "GERADO" = gerado_ba, "MCD45" = mcd45_ba, "MERIS" = meris_ba, "FIRE_CCI5.1" = ba_modis)
+           "GERADO" = gerado_ba, "MCD45" = mcd45_ba, "MERIS" = meris_ba, "FIRE_CCI5.1" = fcci_modis)
 cols =  make_col_vector(c('#1b9e77','#d95f02','#7570b3'), ncols = 5)#'c("blue", "red")
-cols = lighten(cols, c(-0.3, 0.3, -0.3, 0.3, -0.45))
+#cols = lighten(cols, c(-0.3, 0.3, -0.3, 0.3, -0.45))
 cols = c("black", cols)
 
 varss = list("Burnt area (%)" = BAs,
-             "Tree cover (%)"       = c(gfed4s_tree, gfed4s_tree, mcd45_tree, mcd45_tree, meris_tree, gfed4s_tree),
-             "Non-tree cover(%)"    = c(gfed4s_nontree, gfed4s_nontree, mcd45_nontree, mcd45_nontree, meris_nontree, gfed4s_nontree))
-varss[["Total veg cover  (%)"]] =  mapply('+', varss[[1]], varss[[2]])#c(gfed4s_tree + gfed4s_nontree, gfed4s_tree + gfed4s_nontree))
+             "Tree cover (%)" = c(gfed4s_tree, gfed4s_tree, mcd45_tree, mcd45_tree, meris_tree, gfed4s_tree),
+             "Non-tree cover(%)" = c(gfed4s_nontree, gfed4s_nontree, mcd45_nontree, mcd45_nontree, meris_nontree, gfed4s_nontree))
+             varss[["Total veg cover  (%)"]] =  mapply('+', varss[[1]], varss[[2]])#c(gfed4s_tree + gfed4s_nontree, gfed4s_tree + gfed4s_nontree))
+
 
 bins = list(seq(0, 1, 0.01), seq(0, 81, 1), seq(0, 100, 1), seq(0, 100, 1))
-
+scales = rep(1, 4)
+logs = rep('', 4)
 logit_squish <- function(x) {
     x = (x*999 + 0.5) /1000
     x = log(x/(1-x))
 }
-cummalitivePlot <- function(vars, name, bin, yaxis = FALSE, yaxis2 = FALSE,
-                            cummm = TRUE, poly = FALSE, logit = FALSE, diffFrom1st = TRUE) {
+cummalitivePlot <- function(vars, name, bin, scale, yaxis = FALSE, yaxis2 = FALSE,
+                            cummm = TRUE, poly = FALSE, log = '', logit = FALSE, diffFrom1st = TRUE, ...) {
     if (logit) yrange = c(-7.7, 7.7) else yrange = c(0, 1)
-    
-    plot(range(bins), yrange, type = 'n', xaxt ='n', yaxt = 'n', 
-         xlab = '', ylab = '', xlim = c(bin[1], tail(bin, 1)))
+    vars = lapply(vars, function(i) i * scale)
+   
+    plot(range(bins), yrange, type = 'n', xaxt ='n', yaxt = 'n', log = log,
+         xlab = '', ylab = '', xlim = c(bin[1], tail(bin, 1)), ...)
     axis(1)
     mtext(side = 1, line = 2.3, name)
     if (logit) {
@@ -86,13 +89,33 @@ doThePlot <- function(fname, ...) {
     png(fname, height = 7, width = 9, units = 'in', res = 300)
         layout(rbind(1:2, 3:4, 5), heights = c(1, 1, 0.3))
         par(mar = c(3.5, 0.5, 0, 0.5), oma = c(0, 3.4, 1, 3.4))
-        mapply(cummalitivePlot, varss, names(varss), bins, c(T, F, T, F), c(F, T, F, T), MoreArgs = list(...))
+        mapply(cummalitivePlot, varss, names(varss), bins, scales, c(T, F, T, F), c(F, T, F, T), log = logs, MoreArgs = list(...))
         mtext(side = 2, line = 2.3, "Cummulative burnt area (%)", outer = TRUE)
         mtext(side = 4, line = 2.3, "Difference from GFED4 (%)", outer = TRUE)
         plot.new()
         legend('top', col = cols, lwd = 2, legend = names(BAs), horiz = TRUE)
     dev.off()
 }
-doThePlot("figs/bot_cummlative_BA_Diff.png", cummm = FALSE)    
-doThePlot("figs/cummlative_BA_Diff.png")
-doThePlot("figs/cummlative_BA.png", diffFrom1st = FALSE)
+#doThePlot("figs/bot_cummlative_BA_Diff.png", cummm = FALSE)    
+#doThePlot("figs/cummlative_BA_Diff.png")
+#doThePlot("figs/cummlative_BA.png", diffFrom1st = FALSE)
+
+varss = list("Burnt area (%)" = BAs,
+             "Cropland cover (%)" = c(gfed4s_crop, gfed4s_crop, mcd45_crop, mcd45_crop, meris_crop, gfed4s_crop),
+             "Pasture cover(%)" = c(gfed4s_pasture, gfed4s_pasture, mcd45_pasture, mcd45_pasture, meris_pasture, gfed4s_pasture),
+             "Population Density (pop/km2)" =  c(gfed4s_popd, gfed4s_popd, mcd45_popd, mcd45_popd, meris_popd, gfed4s_popd))
+
+scales[2:3] = 100
+bins[[4]] = 10^(seq(-5, 3, 0.01))
+logs[4] = 'x'
+#doThePlot("figs/cummlative_BA_Human_Diff.png")
+
+varss = list("Burnt area (%)" = BAs,
+             "Cropland cover (%)" = c(gfed4s_crop, gfed4s_crop, mcd45_crop, mcd45_crop, meris_crop, gfed4s_crop),
+             "Pasture cover(%)" = c(gfed4s_pasture, gfed4s_pasture, mcd45_pasture, mcd45_pasture, meris_pasture, gfed4s_pasture),
+             "Population Density (pop/km2)" =  c(gfed4s_popd, gfed4s_popd, mcd45_popd, mcd45_popd, meris_popd, gfed4s_popd))
+
+scales[2:3] = 100
+bins[[4]] = 10^(seq(-5, 3, 0.01))
+logs[4] = 'x'
+
